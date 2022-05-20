da1a59b06edcbb6f39537bc9da67a978c1e1f94a
Abdou990F <benari102@gmail.com>
2022-05-20 22:46:00 +0100

    Funny

--- source/why/MyPlace.hx
+++ source/why/MyPlace.hx
@@ -10,11 +10,24 @@ class MyDialog extends MusicBeatState {
     var myDialog:Array<String>;
     var guyDialog:Array<String>;
 
-    private static function talk(who:String, says:String)
+    /**
+	 * This things does things.
+	 *
+	 * @param	Thing		that is a thing.
+	 */
+    var fullDialog:Array<String>;
+
+    private static function say(who:String, says:String)
         {
             //Ha ha, thats funny, idk what but its funny.
-            var dialog:Array = [who, says];
+            var dialog:Array = [who + ': ', says];
+            //trace(dialog);
+            fullDialog = dialog;
+            doFirst();
         }
+    public static doFirst(){
+        trace(fullDialog);
+    }
 }
 
 //This is a joke file. We can say... Maybe...
