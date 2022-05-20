76dd87864dfed06b53d58e8c747e5036cc7e4334
Abdou990F <benari102@gmail.com>
2022-05-20 22:00:04 +0100

    Funny

---
+++ source/IdioticThings/MyDialog.hx
@@ -0,0 +1,20 @@
+// This file is like my playground, I test nothing here,  play with my keyboard (:
+// Also, nothing for the mod is actually here.
+// So if you are searching for a secret thing, there is nothing here.
+
+package why;
+import flixel.FlxState;
+import flixel.FlxG;
+
+class MyDialog extends MusicBeatState {
+    var myDialog:Array<String>;
+    var guyDialog:Array<String>;
+
+    private static function talk(who:String, says:String)
+        {
+            //Ha ha, thats funny, idk what but its funny.
+            var dialog:Array = [who, says];
+        }
+}
+
+//This is a joke file. We can say... Maybe...
